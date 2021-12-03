66ccc1d473d7c653a082a0d88b88450b59a71dac
Ушаков <andrey.ushakov.00@inbox.ru>
2021-12-03 18:20:15 +0300

    review/comments

--- api_yamdb/yamdb/models.py
+++ api_yamdb/yamdb/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Review (models.Model):
+    title_id = models.OneToOneField(
+        Title, on_delete=models.CASCADE, related_name='review'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='review'
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True
+    )
+    score = models.IntegerField( 
+        'оценка',
+        validators=(
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        )
+    )
+
+
+class Comments (models.Model):
+    review_id = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True
+    )
