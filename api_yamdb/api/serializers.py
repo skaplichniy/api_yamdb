2f85b310e2c5c0778769696b0e5ea642b902f44e
skaplichniy <skaplichniy@gmail.com>
2021-12-03 16:38:15 +0300

    category, genre and titles

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from yamdb.models import Category, Genre, Titles
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class TitlesSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer()
+    category = CategorySerializer()
+
+    class Meta:
+        model = Titles
+        fields = '__all__'
--- api_yamdb/yamdb/models.py
+++ api_yamdb/yamdb/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model)
+    CHOICES_CATEGORY = (
+        ('books', 'Книги'),
+        ('movies', 'Фильмы'),
+        ('music', 'Музыка'),
+    )
+    name = models.CharField(max_length=300, choices = CHOICES_CATEGORY)
+    slug = models.SlugField(null=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model)
+    CHOICES_GENRE = (
+        ('story', 'Сказка'),
+        ('rock', 'Рок'),
+        ('arthouse', 'Артхаус'),
+    )
+    name = models.CharField(max_length=300, choices = CHOICES_GENRE)
+    slug = models.SlugField(null=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Titles(models.Model)
+    name = models.CharField(max_length=300)
+    genre = models.ForeignKey(Genre)
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='titles',
+        help_text='Категория произведения',
+        verbose_name='Категория')
+    slug = models.SlugField(null=False, unique=True)
+
+    def __str__(self):
+        return self.name
